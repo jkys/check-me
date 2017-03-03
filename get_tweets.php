c3760c24779189a704e2c164213f46b2a5eab792
ColbyDaly <dalyc2@wit.edu>
2017-03-03 18:01:41 -0500

    php update

--- get_tweets.php
+++ get_tweets.php
@@ -3,15 +3,31 @@
 ini_set('display_errors', 1);
 require_once('TwitterAPIExchange.php');
 
+$user_id = $_POST['userID']
+$oauth_access_token = $_POST['token'];
+$oauth_access_token_secret = $_POST['secret'];
+
 $settings = array(
-    'oauth_access_token' => "602086335-wiQPEmB5JFLrGKYtB48wywuGGkDjOdoRXg7Nn8gO",
-    'oauth_access_token_secret' => "26cOcASxxED31GmzrlELBfnJBKbEBQDwJTxYPb02aOjef",
+    'oauth_access_token' => $oauth_access_token,
+    'oauth_access_token_secret' => $oauth_access_token_secret,
     'consumer_key' => "ifZDKvMyvpP4uzRUPMHACnUwj",
     'consumer_secret' => "K1I63Xpp96eruMsVzhk2S4dZ48tAddQe8Vc7exCTzRi2aJaKwM"
 );
 
+$url = 'https://api.twitter.com/1.1/users/lookup.json';
+$getfield = '?user_id=' . $user_id;
+$requestMethod = 'POST';
+$twitter = new TwitterAPIExchange($settings);
+$response1 = $twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();
+
+$screen_name = $response1.screen_name;
+
+
+
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield = '?screen_name=ColbyDaly&trim_user=true&count=199&exclude_replies=true';
+$getfield = '?screen_name=' . $screen_name . '&trim_user=true&count=199&exclude_replies=true';
 $requestMethod = 'GET';
 $twitter = new TwitterAPIExchange($settings);
 $response = $twitter->setGetfield($getfield)
@@ -24,7 +40,7 @@ $response = $twitter->setGetfield($getfield)
 
 
 
-echo json_encode($response);
+echo json_encode($response1);
 
 
 
