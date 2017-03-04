ae016d307f570ed3b2e7847507b80a555545b257
ColbyDaly <dalyc2@wit.edu>
2017-03-04 12:15:35 -0500

    syntax

--- js/firebaseIntialize.js
+++ js/firebaseIntialize.js
@@ -284,19 +284,16 @@ $( document ).ready(function() {
 
 								if (typeof response.errors === 'undefined' || response.errors.length < 1) {
 									
-									var profanityObj = {word: null,
-														category: null,
-														scale: 0};
 
 									console.log(response);
 									i = 0; 
 									$.each(JSON.parse(response), function(i, obj) {
 
-										var profanityObj .= i = {word: obj.Word,
+										var "profanityObj" + i = {word: obj.Word,
 																category: obj.category,
 																scale: obj.scale
 																}
-										console.log(profanityObj . i);
+										console.log(profanityObj2);
 
 
 										// $tweets.append('<li>' + "Created at: " + obj.created_at + " Message: " + obj.text + " ID: " + obj.id + '</li>');
