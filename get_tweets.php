afd885bfcd8a3e254d087cc28187961d6ed0853b
Jonathan <keysj@wit.edu>
2017-03-06 00:04:29 -0500

    Twitter testing

--- get_tweets.php
+++ get_tweets.php
@@ -22,12 +22,19 @@ $json_user = json_decode($user, true);
 $screen_name = $json_user[0]['screen_name'];
 
 $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-$getfield = '?screen_name=' . $screen_name . '&trim_user=true&count=199&exclude_replies=true&page=4';
-$tweets = 
-    $twitter
-    ->setGetfield($getfield)
-    ->buildOauth($url, 'GET')
-    ->performRequest();
+$getfield = '?screen_name=' . $screen_name . '&trim_user=true&count=200&exclude_replies=true&page=4';
+$page = 1;
+
+while($tweet != '' | $page > 5) {
+    $getfield = '?screen_name=' . $screen_name . '&trim_user=true&count=200&exclude_replies=true&page=' . $page;
+    $tweet = 
+        $twitter
+        ->setGetfield($getfield)
+        ->buildOauth($url, 'GET')
+        ->performRequest();
+    ++$page;
+    $tweets += $tweet;
+}
 
 echo json_encode($tweets);
 ?>
