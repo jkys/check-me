857f2eaf2b22a657472a8ef7f4cbe6429f7ef5bc
ColbyDaly <dalyc2@wit.edu>
2017-04-01 14:29:07 -0400

    fb comments

--- js/firebaseIntialize.js
+++ js/firebaseIntialize.js
@@ -130,23 +130,16 @@ $( document ).ready(function() {
 		console.log(response);
 		if (response && !response.error && response.data != '') {
 			response.posts.data.forEach(function(data) {
-				var post = data.message;
-				var url = getFaceBookPostUrl(data.id);
-				var date = convertIso(data.created_time);
-				displayPost(post, date, url, 'facebook');
+				response.posts.data.comments.data.forEach(function(data) {
+					var post = data.message;
+					var url = getFaceBookPostUrl(data.id);
+					var date = convertIso(data.created_time);
+					displayPost(post, date, url, 'facebook');
+				})
 			})
 	    	//FB.api(response.paging.next, getPosts);
 		}
-		if (response && !response.error && response.data != '') {
-			response.posts.data.comments.data.forEach(function(data) {
-				var post = data.message;
-				var url = getFaceBookPostUrl(data.id);
-				var date = convertIso(data.created_time);
-				displayPost(post, date, url, 'facebook');
-				console.log(data);
-			});
-	    	//FB.api(response.paging.next, getPosts);
-		}
+
 	}
 
 	function getFacebookPosts(token) {
