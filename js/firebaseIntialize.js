e061af8b674725a0d4bb1dca5d11d9dd245ba106
ColbyDaly <dalyc2@wit.edu>
2017-04-01 15:13:28 -0400

    fb comments

--- js/firebaseIntialize.js
+++ js/firebaseIntialize.js
@@ -225,7 +225,7 @@ $( document ).ready(function() {
 							imgUrl = array.photoURL;
 						}
 					});
-					$('#' + platform + 'Results').append('<button class="postButton"><div class="post"><img src="' + imgUrl + '" class="postImg"><h3 class="time">' + from + '</h3><h3 class="time">' + date + '</h3><p class="text">' + message + '</p><div class="reasons"><hr>Flagged words in post: ' + flaggedWords.slice(0, -2) + '<br>Flagged words: ' + (score/100) + '.<br><a href="' + url + '" class="postLink">Click here to navigate to post.</a></div></div></button>');
+					$('#' + platform + 'Results').append('<button class="postButton"><div class="post"><img src="' + imgUrl + '" class="postImg"><h3 class="time">' + from + '</h3><br><h3 class="time">' + date + '</h3><p class="text">' + message + '</p><div class="reasons"><hr>Flagged words in post: ' + flaggedWords.slice(0, -2) + '<br>Flagged words: ' + (score/100) + '.<br><a href="' + url + '" class="postLink">Click here to navigate to post.</a></div></div></button>');
 				}
 			});
 		}
