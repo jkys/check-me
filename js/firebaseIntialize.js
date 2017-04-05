30dd743a40e84063572f3e795aee264286b3f4f4
ColbyDaly <dalyc2@wit.edu>
2017-04-04 22:21:19 -0400

    bug fix

--- js/firebaseIntialize.js
+++ js/firebaseIntialize.js
@@ -391,13 +391,7 @@ $( document ).ready(function() {
 		}).catch(function(error) {
 			outPutMessage('register', false, error.message);
 		});
-		/*
-		window.location = 'settings.html';
-		$("#tab-content" ).tabs( { 
-			active: 1 
-		} );
-		alert("Please authorize at least 1 social media platform");
-		*/
+	});
 
 	$('#unlinkFacebook').click(function() {
 		var result = unlinkAccounts(auth.currentUser, 'facebook');
