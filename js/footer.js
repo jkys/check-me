84140f7d2cf180025b0f89a7136fd0d339f95ac2
Jonathan <keysj@wit.edu>
2017-04-06 16:28:12 -0400

    Fixed bottom bar

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
-	var documentHeight = $(document).height();
-	var distanceFromTop = $('footer').offset().top;
-	var marginTop = documentHeight - distanceFromTop;
-	$('body').height(documentHeight);
-	$('footer').css('margin-top', marginTop + 45);
-});
+// $( document ).ready(function() {
+// 	var documentHeight = $(document).height();
+// 	var distanceFromTop = $('footer').offset().top;
+// 	var marginTop = documentHeight - distanceFromTop;
+// 	$('body').height(documentHeight);
+// 	$('footer').css('margin-top', marginTop + 45);
+// });
