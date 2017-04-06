739e096eea452ee641f2d63fd5c7c55e1ea12e48
Jonathan <keysj@wit.edu>
2017-04-06 18:48:37 -0400

    small fix

--- js/firebaseIntialize.js
+++ js/firebaseIntialize.js
@@ -214,6 +214,8 @@ $( document ).ready(function() {
 			var score = 0;
 			var flaggedWords = '';
 			var profaneJson = firebase.database().ref('Profanity');
+
+			var link = "onclick=\"window.open(url, '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');\"";
 			profaneJson.on('value', function(snapshot) {
 				profaneWordSet = snapshot.val();
 				profaneWordSet.forEach(function(profaceInnerJson) {
@@ -236,7 +238,7 @@ $( document ).ready(function() {
 							imgUrl = array.photoURL;
 						}
 					});
-						$('#' + platform + 'Results').append('<button class="postButton"><div class="post"><img src="' + imgUrl + '" class="postImg"><div class="timename"><h3 class="time">' + from + '</h3><h3 class="time">' + date + '</h3></div><p class="text">' + message + '</p><div class="reasons"><hr>Flagged words in post: ' + flaggedWords.slice(0, -2) + '<br>Flagged words: ' + (score/100) + '.<br><a href="' + url + '" class="postLink">Click here to navigate to post.</a></div></div></button>');
+						$('#' + platform + 'Results').append('<button class="postButton"><div class="post"><img src="' + imgUrl + '" class="postImg"><div class="timename"><h3 class="time">' + from + '</h3><h3 class="time">' + date + '</h3></div><p class="text">' + message + '</p><div class="reasons"><hr>Flagged words in post: ' + flaggedWords.slice(0, -2) + '<br>Flagged words: ' + (score/100) + '.<br><p ' + link + '>Click here to navigate to post.</p></div></div></button>');
 					}
 					else{
 											auth.currentUser.providerData.forEach(function(array) {
@@ -404,10 +406,6 @@ $( document ).ready(function() {
 				return false; 
 			}
 		});
-
-		prefix.forEach(function(ending) {
-
-	});
 	}
 
 
