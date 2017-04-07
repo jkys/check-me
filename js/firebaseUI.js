38125a024e34eeb3e51a61c6e11166905b2c0422
Jonathan <keysj@wit.edu>
2017-04-07 03:24:39 -0400

    BIO update

--- js/firebaseUI.js
+++ js/firebaseUI.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 	// FirebaseUI config.
 	var uiConfig = {
-		signInSuccessUrl: 'home.html',
+		signInSuccessUrl: 'www.checkyou.me/home.html',
 		signInOptions: [
 			// Leave the lines as is for the providers you want to offer your users.
 			firebase.auth.FacebookAuthProvider.PROVIDER_ID,
